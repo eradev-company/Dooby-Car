f23822f7606b1e9d6c8354b7b14e56ed011778af
Casthash532 <gm_sahraoui@esi.dz>
2022-04-27 14:22:55 +0100

    migrate to ros galactic + some fixes

--- ROS/pi-node/ros_entrypoint.sh
+++ ROS/pi-node/ros_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-source /opt/ros/foxy/setup.bash
+source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI=file:///app/cyclonedds.xml
